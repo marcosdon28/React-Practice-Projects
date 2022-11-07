200eaf4f89f8e2ae0086fdc7d256bdb40fdabb7a
marcosdon28 <marcosdon28@gmail.com>
2022-11-07 06:43:55 -0300

    modify usestate hook practice

--- usestate-hook-practice/src/components/App.jsx
+++ usestate-hook-practice/src/components/App.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 
 function App() {
-  const now = new Date().toLocaleTimeString();
-  const [time, setTime] = useState(now);
+  var now = new Date().toLocaleTimeString();
+  var [time, setTime] = useState(now);
+
+  setInterval(changeTime , 1000)
+
+  function changeTime (){
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime)
+  }
 
   return (
     <div className="container">
       <h1>{time}</h1>
-      <button>Get Time</button>
+      <button onClick={changeTime}>Get Time</button>
     </div>
   );
 }
